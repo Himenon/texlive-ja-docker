8cfd09c6074f9bfc3e649befe37ced2a2e5c5090
Himenon <k.himeno314@gmail.com>
2022-05-29 20:13:40 +0900

    chore: update

--- scripts/docker-cleanup.mjs
+++ scripts/docker-cleanup.mjs
@@ -1,4 +1,7 @@
-import pkg from "../package.json"
+import "zx/globals";
+import fs from "fs";
+
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
 
 const scope = pkg.docker.scope;
 const imageName = pkg.docker.imageName;
